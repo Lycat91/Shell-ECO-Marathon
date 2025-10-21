543ead254c631bdd9be1b82db70b671068bdce8c
Lycat91 <158769561+Lycat91@users.noreply.github.com>
2025-10-21 09:46:09 -0500

    comment

--- UART_Test.c
+++ UART_Test.c
@@ -3,24 +3,34 @@
 #include <stdio.h>
 #include <string.h>
 
-#define UART_ID uart0
-#define TX_PIN 0
-#define RX_PIN 1
+#define UART_ID   uart1
+#define TX_PIN    4        // UART1 TX (default pin)
+#define RX_PIN    5        // UART1 RX (default pin)
 #define BAUD_RATE 115200
 
 int main() {
     stdio_init_all();
+
+    // Initialize UART1
     uart_init(UART_ID, BAUD_RATE);
     gpio_set_function(TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(RX_PIN, GPIO_FUNC_UART);
-    int counter =0;
 
+    // Optional: disable buffering for immediate output
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    int counter = 0;
     char message[64];
 
     while (true) {
-  
-        counter += 1;
-        sprintf(message, "counter=%d\n", counter);
+        counter++;
+
+        // Print to USB console (if enabled in CMake)
+        printf("Counter (USB): %d\n", counter);
+
+        // Send over UART1 to the other Pico
+        snprintf(message, sizeof(message), "counter=%d\n", counter);
+        printf(message);
         uart_puts(UART_ID, message);
 
         sleep_ms(250);
